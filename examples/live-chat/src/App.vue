12e3368924569be1cbd1a9f800a1995bad0c61db
Pablo Betancurt <pablo.betancurt@fing.edu.uy>
2022-12-20 11:22:01 -0300

    Improved message when credentials are not settled (#27)
    * Improved message when credentials are not settled
    
    * Increased modal's size

--- examples/live-chat/src/App.vue
+++ examples/live-chat/src/App.vue
@@ -1,13 +1,15 @@
 <template>
+  <b-modal size="lg" v-model="modalShow" v-if="!pubnubSettled || !millicastSettled" ref="my-modal" title="Warning" ok-only>
+    <ul>
+      <li v-if="!pubnubSettled">You have not settled your PubNub credentials in the .ENV file.</li>
+      <li v-if="!millicastSettled">You have not settled your Millicast credentials in the .ENV file.</li>
+    </ul>
+  </b-modal>
+
   <div id="container" class="row">
     <div id="video-player" class="col d-flex align-items-center py-3">
-      <template v-if="streamId.accountId && streamId.streamName">
+      <template v-if="millicastSettled">
         <VideoPlayer :paramsOptions="streamId" class="video-container" />
-      </template>
-      <template v-else>
-        <div class="alert alert-danger fade show" role="alert">
-          You have not settled your Millicast credentials in the .ENV file.
-        </div>
       </template>
     </div>
     <LiveChat />
@@ -16,6 +18,8 @@
 
 <script>
 import LiveChat from './components/LiveChat.vue'
+const PubNubCredentials = process.env.VUE_APP_PUBNUB_PUBLISH_KEY && process.env.VUE_APP_PUBNUB_SUBSCRIBE_KEY && process.env.VUE_APP_PUBNUB_UUID
+const MillicastCredentials = process.env.VUE_APP_MILLICAST_ACCOUNT_ID && process.env.VUE_APP_MILLICAST_STREAM_NAME
 export default {
   name: 'App',
   components: {
@@ -27,6 +31,9 @@ export default {
         accountId: process.env.VUE_APP_MILLICAST_ACCOUNT_ID,
         streamName: process.env.VUE_APP_MILLICAST_STREAM_NAME,
       },
+      pubnubSettled: PubNubCredentials,
+      millicastSettled: MillicastCredentials,
+      modalShow: true
     }
   },
 }
