f1e02b38b4457debe32cb89548a39283c6a88389
Santiago Souto <Santiago.Souto@dolby.com>
2024-01-23 19:18:44 +0100

    Fix/Firefox stats not showing all values (#84)
    Change the way values are parsed when stats event is called. Before,
    stats used `mid` value to select which stats to show. Now it is based on
    the `trackIdentifier` of the stats event's payload, that is available in
    all browsers.
    
    On Firefox, when viewing the Media Stats, it should be shown video stats
    that previously were not shown.
    
    This fix depends on a change from the Millicast SDK.

--- src/components/VideoPlayerStatsTable.vue
+++ src/components/VideoPlayerStatsTable.vue
@@ -149,15 +149,17 @@ export default {
       stats: {},
       statsIndex: 0,
       selectedSourceMid: null,
-      midToStatsIndexMap: {},
+      trackIdToStatsIndexMap: {},
+      trackIdMidMap: {}
     }
   },
   mounted() {
     this.millicastView.webRTCPeer.initStats()
     this.millicastView.webRTCPeer.on('stats', (peerStats) => {
+      this.trackIdMidMap = this.getTrackIdMidMap
       peerStats.video?.inbounds?.forEach((stat, index) => {
-        if (stat.mid) {
-          this.midToStatsIndexMap[stat.mid] = index
+        if (stat.trackIdentifier) {
+          this.trackIdToStatsIndexMap[stat.trackIdentifier] = index
         }
       })
       window.peer?.getReceivers?.().forEach?.((receiver) => {
@@ -190,7 +192,8 @@ export default {
     },
     handleSourceChange() {
       const mid = this.selectedSourceMid ?? 0
-      this.statsIndex = this.midToStatsIndexMap[mid]
+      const trackId = this.trackIdMidMap[mid]
+      this.statsIndex = this.trackIdToStatsIndexMap[trackId]
     },
     selectMidZero() {
       this.selectedSourceMid = this.getTransceiverSourceState[0]?.mid 
@@ -210,7 +213,8 @@ export default {
       'videoSources'
     ]),
     ...mapGetters('Sources', [
-      'getTransceiverSourceState'
+      'getTransceiverSourceState',
+      'getTrackIdMidMap'
     ]),
     hasStats() {
       return Object.keys(this.stats).length > 0
@@ -226,7 +230,9 @@ export default {
       const video = this.stats.video?.inbounds
       const videoLength = video?.length
       if (videoLength) {
-        return video[this.midToStatsIndexMap[this.selectedSourceMid]]
+        const trackId = this.trackIdMidMap[this.selectedSourceMid]
+        const statsIndex = this.trackIdToStatsIndexMap[trackId]
+        return video[statsIndex]
       }
       return null
     },
@@ -282,7 +288,7 @@ export default {
       const multiviewIsOn = (
         this.videoSources.length > 1 && 
         this.isSplittedView && 
-        Object.keys(this.midToStatsIndexMap).length
+        Object.keys(this.trackIdToStatsIndexMap).length
       )
       if (!multiviewIsOn) {
         this.selectMidZero()
