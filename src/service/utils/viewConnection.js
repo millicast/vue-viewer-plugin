25a504e855cfc7cb24aeb477f684ec8683c2ec4d
Santiago Souto <ssout@dolby.com>
2024-06-03 16:22:09 -0300

    Change onMetadata event handler

--- src/service/utils/viewConnection.js
+++ src/service/utils/viewConnection.js
@@ -124,18 +124,7 @@ export const setTrackEvent = () => {
   })
 
   if (state.Params.viewer.metadata) {
-    millicastView.on('onMetadata', (event) => {
-      const decoder = new TextDecoder()
-      const metadata = event.metadata
-      metadata.track = event.track
-      const uuid = metadata.uuid
-      metadata.uuid = uuid.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '')
-      metadata.uuid = metadata.uuid.replace(/(.{8})(.{4})(.{4})(.{4})(.{12})/, "$1-$2-$3-$4-$5")
-      if (metadata.timecode) {
-        metadata.timecode = decoder.decode(metadata.timecode)
-      } else if (metadata.unregistered) {
-        metadata.unregistered = decoder.decode(metadata.unregistered)
-      }
+    millicastView.on('onMetadata', (metadata) => {
       const metadataEvent = new CustomEvent("metadata", { detail: { metadata } })
       window.dispatchEvent(metadataEvent)
     })
